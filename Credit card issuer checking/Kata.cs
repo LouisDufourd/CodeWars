2aa436b3555ae100a40ee2980293d2a32ae8e87f
LouisDufourd <102533233+LouisDufourd@users.noreply.github.com>
2025-06-24 13:38:06 +0200

    Ajout du kata Credit cars issuer checking

---
+++ Credit card issuer checking/Kata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Kata
+{
+    public static string getIssuer(long number)
+    {
+        string num = $"{number}";
+
+        Console.WriteLine(num.Length);
+
+        if (num.StartsWith("4") && (num.Length == 13 || num.Length == 16)) return "VISA";
+        else if (num.StartsWith("6011") && num.Length == 16) return "Discover";
+        else if ((num.StartsWith("34") || num.StartsWith("37")) && num.Length == 15) return "AMEX";
+        else if (
+            (num.StartsWith("51") ||
+            num.StartsWith("52") ||
+            num.StartsWith("53") ||
+            num.StartsWith("54") ||
+            num.StartsWith("55")) && num.Length == 16) return "Mastercard";
+        else
+        {
+            Console.WriteLine(num);
+            return "Unknown";
+        }
+    }
+}
---
+++ Credit card issuer checking/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine(Kata.getIssuer(5879729091));
+Console.WriteLine(Kata.getIssuer(3875552667067));
+Console.WriteLine(Kata.getIssuer(2095741855529));
+Console.WriteLine(Kata.getIssuer(1667238490125));
+Console.WriteLine(Kata.getIssuer(3914834749605));
+Console.WriteLine(Kata.getIssuer(2692211021435));
+Console.WriteLine(Kata.getIssuer(2659204904277));

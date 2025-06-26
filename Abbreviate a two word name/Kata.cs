3ca84fd38d290d003bee2b154a602870ef4052ae
LouisDufourd <102533233+LouisDufourd@users.noreply.github.com>
2025-06-26 14:13:00 +0200

    Ajout de la classe Kata avec méthode AbbrevName
    Implémentation d'une méthode statique `AbbrevName` dans la classe `Kata` qui génère une abréviation à partir d'un nom en prenant la première lettre de chaque mot, en les mettant en majuscule et en ajoutant un point. Le dernier point est supprimé avant de retourner le résultat.

---
+++ Abbreviate a two word name/Kata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Kata
+{
+    public static string AbbrevName(string name)
+    {
+        var spaces = name.Split(' ');
+        var res = "";
+        foreach (var s in spaces)
+        {
+            res += char.ToUpper(s[0]) + ".";
+        }
+
+        res = res.Remove(res.Length - 1);
+        return res;
+    }
+}

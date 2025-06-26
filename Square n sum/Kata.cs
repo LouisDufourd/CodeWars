48646f49a5bc9747f870a5ee0a5556c08c2101e4
LouisDufourd <102533233+LouisDufourd@users.noreply.github.com>
2025-06-26 16:33:08 +0200

    Ajout du projet "Your order please" et fonctionnalités
    - Création du projet "Your order please" dans `CodeWars.sln`.
    - Ajout d'une méthode `SquareSum` dans `Kata.cs`.
    - Création du fichier `kata.cs` avec un espace de noms approprié.
    - Ajout d'un message "Hello, World!" dans `Program.cs`.
    - Configuration du projet dans `Your order please.csproj`.

--- Square n sum/Kata.cs
+++ Square n sum/Kata.cs
@@ -6,5 +6,6 @@ using System.Threading.Tasks;
 
 public static class Kata
 {
+    private static char[] chars = { 'a', 'e', 'i', 'o', 'u' };
     public static int SquareSum(int[] numbers) => (int) numbers.Sum(x => Math.Pow(x, 2));
 }

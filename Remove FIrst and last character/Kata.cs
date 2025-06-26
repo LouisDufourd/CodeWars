42aed84bacce1da1fdadcddd913c6ae684ec61be
LouisDufourd <102533233+LouisDufourd@users.noreply.github.com>
2025-06-26 13:57:42 +0200

    Ajout de nouveaux projets et méthodes dans CodeWars
    Plusieurs projets ont été ajoutés au fichier de solution `CodeWars.sln`, notamment "Unique in order" et "Remove FIrst and last character".
    
    Une nouvelle méthode `Remove_char` a été ajoutée à la classe `Kata` dans `Kata.cs`, permettant de supprimer le premier et le dernier caractère d'une chaîne.
    
    De plus, la méthode `UniqueInOrder` a été implémentée dans `kata.cs`, retournant une liste d'éléments uniques dans l'ordre d'apparition.
    
    Deux nouveaux fichiers de projet, `Remove FIrst and last character.csproj` et `Unique in order.csproj`, ont été créés avec les propriétés appropriées.

---
+++ Remove FIrst and last character/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Kata
+{
+    public static string Remove_char(string s)
+    {
+        string res = s.Remove(0, 1);
+        res = res.Remove(res.Length - 1, 1);
+        return res;
+    }
+}
---
+++ Unique in order/kata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Kata
+{
+    public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+    {
+        if(iterable.Count() < 1) return new List<T>();
+        T previous = iterable.ElementAt(0);
+        List<T> result = new List<T>();
+
+        for (int i = 1; i < iterable.Count(); i++)
+        {
+            var element = iterable.ElementAt(i);
+            if (!previous.Equals(element))
+            {
+                result.Add(previous);
+                previous = element;
+            }
+        }
+
+        result.Add(previous);
+
+        //your code here...
+        return result;
+    }
+}

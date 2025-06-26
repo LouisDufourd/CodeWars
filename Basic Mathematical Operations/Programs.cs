d338b53033c17fa9d64adb21fb9364c2c317b5db
LouisDufourd <102533233+LouisDufourd@users.noreply.github.com>
2025-06-26 14:09:04 +0200

    Ajout de nouveaux projets et fonctionnalités mathématiques
    Ajout des projets "Basic Mathematical Operations" et "Abbreviate a two word name" au fichier de solution `CodeWars.sln`, avec les configurations de build appropriées.
    
    Création des fichiers de projet pour ces nouveaux projets, ciblant le framework .NET 8.0 et définissant le type de sortie comme une application exécutable.
    
    Ajout d'une classe statique `Program` dans `Programs.cs`, incluant la méthode `basicOp` pour effectuer des opérations mathématiques de base.

---
+++ Basic Mathematical Operations/Programs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    public static class Program
+    {
+        public static double basicOp(char operation, double value1, double value2)
+        {
+            switch(operation)
+            {
+                case '+':
+                    return value1 + value2;
+                case '-':
+                    return value1 - value2;
+                case '/':
+                    return value1 / value2;
+                case '*':
+                    return value1 * value2;
+                default:
+                    return -1;
+            }
+        }
+    }
+}
